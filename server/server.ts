d666665c17c9724091000827b97c844b007f606a
Kevin Huynh <kvhuynh820@gmail.com>
2025-12-05 14:25:04 -0800

    test

--- server/server.ts
+++ server/server.ts
@@ -14,7 +14,9 @@ const { mapRouter } = require("./routes/maps.routes")
 app.use(express.json());
 
 // local server
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+// app.use(cors({ credentials: true, origin: "http://localhost:5173" })); //test
+app.use(cors({ credentials: true, origin: "https://jordanlab-backend.onrender.com" })); // prod
+
 
 // app.use(cors({ credentials: true, origin: "*" }));
 
