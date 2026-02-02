15a6b9cb4f5ebb8eb984d8a7cd9471a3784e7f56
Kevin Huynh <kvhuynh820@gmail.com>
2026-02-02 12:50:54 -0800

    added tristan's bio:

--- client/src/data/people.ts
+++ client/src/data/people.ts
@@ -16,10 +16,9 @@ export const people: Record<
 			name: "Tristan Jordan",
 			role: "Principal Investigator",
 			email: "txjordan@uw.edu",
-			image:
-				"./images/people/tristan/tristan-cropped-1x1.jpg",
+			image: "./images/people/tristan/tristan-cropped-1x1.jpg",
 			description:
-				"Research focus description goes here. Could be 2–3 sentences about PI.",
+				"I was born and raised in Virginia Beach, VA. Despite loving The South, warmth, and sunshine, my academic career has taken me north(west)ward where I have been excited to launch my lab at The University of Washington. I completed my bachelor’s degree at The College of Wooster in Wooster, Ohio with a double major in Biology and Classical Studies. Afterwards I joined a NIH Postbaccalaureate Research Education Program (PREP) at The University of Chicago in the lab of Dr. Glenn Randall. After completion of PREP, I continued in Dr. Randall’s lab for my PhD where I studied the signaling pathways that control lipid metabolism (specifically the autophagic degradation of lipid droplets) during dengue virus infection. From Chicago, I moved to New York where I joined the lab of Dr. Benjamin tenOever (first at the Icahn School of Medicine at Mount Sinai and then at NYU Grossman School of Medicine) as a postdoc. While I spent significant time working on SARS-CoV-2, it is in Dr. tenOever’s lab that I discovered my love of eukaryotic antiviral immunity - the evolution of diverse antiviral strategies, how viruses evade these, and the consequences of these antiviral strategies to the host. Since starting the lab in 2023, this has been our focus and I cannot be happier. While Seattle is certainly different from New York, Chicago, or the South, I’ve enjoyed the transition to the PNW and am constantly amazed at how gorgeous it is out here. When I am not science-ing, you can find me reading, playing video games, or enjoying the beautiful outdoors of the PNW.",
 			links: [{ label: "Publications", orcId: "#" }],
 		},
 	],
@@ -29,8 +28,7 @@ export const people: Record<
 			name: "Freya van't veer",
 			role: "3rd year graduate student",
 			email: "freyav@uw.edu",
-			image:
-				"./images/people/freya/freya-cropped-1x1.jpg",
+			image: "./images/people/freya/freya-cropped-1x1.jpg",
 			description:
 				"Research focus description goes here. Could be 2–3 sentences about PI.",
 			links: [{ label: "Publications", orcId: "" }],
@@ -39,8 +37,7 @@ export const people: Record<
 			name: "Maria McCavitt-Malvido",
 			role: "2nd year graduate student",
 			email: "mfmm@uw.edu",
-			image:
-				"./images/people/maria/maria.jpg",
+			image: "./images/people/maria/maria.jpg",
 			description:
 				"Research focus description goes here. Could be 2–3 sentences about PI.",
 			links: [{ label: "Publications", orcId: "#" }],
@@ -49,8 +46,7 @@ export const people: Record<
 			name: "Hunter Toyoda",
 			role: "2nd year graduate student",
 			email: "htoyoda@uw.edu",
-			image:
-				"./images/people/hunter/hunter-swag-cropped-1x1.jpg",
+			image: "./images/people/hunter/hunter-swag-cropped-1x1.jpg",
 			description:
 				"Research focus description goes here. Could be 2–3 sentences about PI.",
 			links: [{ label: "Publications", orcId: "#" }],
@@ -61,8 +57,7 @@ export const people: Record<
 			name: "Avi Shukla",
 			role: "Staff Scientist",
 			email: "ashukl3@uw.edu",
-			image:
-				"./images/people/avi/avi-cropped-1x1.jpg",
+			image: "./images/people/avi/avi-cropped-1x1.jpg",
 			description:
 				"Research focus description goes here. Could be 2–3 sentences about PI.",
 			links: [{ label: "Publications", orcId: "#" }],
@@ -71,8 +66,7 @@ export const people: Record<
 			name: "Bernie Sentigar",
 			role: "Lab Manager/Research Scientist",
 			email: "bsenti@uw.edu",
-			image:
-				"./images/people/bernie/bernie-cropped-1x1.jpg",
+			image: "./images/people/bernie/bernie-cropped-1x1.jpg",
 			description:
 				"Research focus description goes here. Could be 2–3 sentences about PI.",
 			links: [{ label: "Publications", orcId: "#" }],
@@ -81,9 +75,9 @@ export const people: Record<
 			name: "Kevin Huynh",
 			role: "Research Scientist",
 			email: "kvhuynh@uw.edu",
-			image:
-				"./images/people/kevin/kevin.jpeg",
-			description: "Kevin was born and raised in Washington and earned a B.S. in Molecular, Cellular, and Developmental Biology from the University of Washington in 2021. He conducted research in the Parrish Lab, studying the molecular mechanisms of neuropathic pain. Since then, his research interests have expanded to include computational methods for biological discovery, and his current work in the lab focuses on identifying antiviral proteins in Eukarya using structural and sequence-based homology approaches at scale. He also made this website 🤓.",
+			image: "./images/people/kevin/kevin.jpeg",
+			description:
+				"Kevin was born and raised in Washington and earned a B.S. in Molecular, Cellular, and Developmental Biology from the University of Washington in 2021. He conducted research in the Parrish Lab, studying the molecular mechanisms of neuropathic pain. Since then, his research interests have expanded to include computational methods for biological discovery, and his current work in the lab focuses on identifying antiviral proteins in Eukarya using structural and sequence-based homology approaches at scale. He also made this website 🤓.",
 			links: [{ label: "Publications", orcId: "0009-0000-6920-9935" }],
 		},
 	],
@@ -92,8 +86,7 @@ export const people: Record<
 			name: "Brandon Duong",
 			role: "Undergraduate",
 			email: "bduong03@uw.edu",
-			image:
-				"./images/people/brandon/brandon-cropped-1x1.jpg",
+			image: "./images/people/brandon/brandon-cropped-1x1.jpg",
 			description:
 				"Research focus description goes here. Could be 2–3 sentences about PI.",
 			links: [{ label: "Publications", orcId: "#" }],
@@ -119,6 +112,5 @@ export const people: Record<
 				"Research focus description goes here. Could be 2–3 sentences about PI.",
 			links: [{ label: "Publications", orcId: "#" }],
 		},
-		
 	],
 };
